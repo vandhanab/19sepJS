8b19b2a98d326a3b28da92333ff3f1085deb9070
vandhanab <112164783+vandhanab@users.noreply.github.com>
2024-09-22 22:32:29 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,16 @@
+var a=10;
+console.log(a)
+
+{}  //block it use where js engine is expected to have single line,
+      // there we can use multiple lines.
+      let a1SI="gabbarsingh";
+      const a2="vandy";
+      {
+        let b=20;
+        console.log(b);
+        console.log(a);
+      }
+      {
+        const c=30;
+        console.log(c);
+      }
